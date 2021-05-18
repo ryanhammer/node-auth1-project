d6d4e14793df6dd7baf07891c55769a8e8b651ab
rhammer35 <rhammer35@icloud.com>
2021-05-17 23:50:58 -0400

    updated server.js to support sessions

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+
 const cors = require("cors");
 
 /**
@@ -14,9 +17,28 @@ const cors = require("cors");
   The session can be persisted in memory (would not be adecuate for production)
   or you can use a session store like `connect-session-knex`.
  */
+  const sessionConfig = {
+    name: 'chocolatechip',
+    secret: 'keep it secret, keep it safe!',
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+      secure: false, // if true the cookie is not set unless it's an https connection
+      httpOnly: false, // if true the cookie is not accessible through document.cookie
+    },
+    rolling: true,
+    resave: false, // some data stores need this set to true
+    saveUninitialized: false, // privacy implications, if false no cookie is set on client unless the req.session is changed
+    store: new KnexSessionStore({
+      knex: require('../database/db-config.js'), // configured instance of knex
+      tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+      sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+      createtable: true, // if the table does not exist, it will create it automatically
+      clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+    }),
+  };
 
 const server = express();
-
+server.use(session(sessionConfig));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
